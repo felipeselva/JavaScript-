e5cb14dadfd56bb4c8b3b1084b6a676dfd5ec664
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-04-03 11:35:11 -0300

    Método Splice

---
+++ Arrays/aula02.js
@@ -0,0 +1,10 @@
+// SPLICE
+
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+
+// nomes.splice(indice, delete)
+//const removidos = nomes.splice(3, 1);
+//const removidos = nomes.splice(3, Number.MAX_VALUE);
+// PUSH = const removidos = nomes.splice(nomes.length, 0, 'Luiz', 'Felipe');
+// SHIFT = const removidos = nomes.splice(0 , 1);
+console.log(nomes, removidos);
