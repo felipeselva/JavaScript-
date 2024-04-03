aa64258703f001b83af33682e22681d3854236f3
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-04-03 10:00:39 -0300

    Revisão do basico em arrays

---
+++ Arrays/aula01.js
@@ -0,0 +1,16 @@
+//const nomes = ['Eduardo', 'Maria', 'Joana'];
+//const novo = nomes.slice(1,3);
+// DELETA UM ELEMENTO DO VETOR = delete nomes [2];
+// ADICIONA UM ELEMENTO AO VETOR = nomes.push('João')
+// RETIRA UM ELEMENTO DO FINAL DO VETOR = novo.pop();
+// RETIRA UM ELEMENTO DO INICIO DO VETOR = nomes.shift();
+// ADICIONA UM ELEMENTO NO INICIO DO VETOR = nomes.unshift('Felipe');
+
+
+// ADICIONA ELEMENTOS DE UMA STRING EM ELEMENTOS DE UMA ARRAY A PARTIR DO PARAMETRO ADICIONADO const nomes = nome.split(' ');
+const nomes = [ 'Felipe', 'Selva', 'Rocha' ]
+// RETORNA ELEMENTOS DE UM ARRAY EM UMA STRING const nome = nomes.join(' ');
+
+console.log(nome);
+
+
