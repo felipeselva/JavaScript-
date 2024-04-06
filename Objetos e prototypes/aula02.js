53e53f47d1c8a2f304944ca58944fd459aae15d1
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-04-06 19:09:41 -0300

    DEFINE-PROPERTY E DEFINE-PROPERTIES

---
+++ Objetos e prototypes/aula02.js
@@ -0,0 +1,39 @@
+    //  DEFINE-PROPERTY E DEFINE-PROPERTIES
+
+    
+            // DEFINE-PROPERTIES
+/*
+        Object.defineProperties(this,{
+            nome: {
+                enumerable:true, // --> MOSTRA A CHAVE
+                value: nome, // -- > VALOR DA CHAVE
+                writable: true, // --> TRUE = PODE ALTERAR VALORES
+                configurable : true // --> TRUE = CONFIGURAR VARIAVEL  
+            },
+            preco: {
+                enumerable:true, // --> MOSTRA A CHAVE
+                value: preco, // -- > VALOR DA CHAVE
+                writable: true, // --> TRUE = PODE ALTERAR VALORES
+                configurable : true // --> TRUE = CONFIGURAR VARIAVEL  
+            },
+        });
+    }
+*/
+
+        //  DEFINE-PROPERTY
+
+function Produto(nome,preco,estoque) {
+this.nome = nome;
+this.preco=preco;
+
+
+Object.defineProperty(this, 'estoque', {
+    enumerable:true, // --> MOSTRA A CHAVE
+    value: estoque, // -- > VALOR DA CHAVE
+    writable: true, // --> TRUE = PODE ALTERAR VALORES
+    configurable : true // --> TRUE = CONFIGURAR VARIAVEL  
+});
+}
+
+const p1 = new Produto ('Camiseta', 20,3)
+    console.log(p1)
