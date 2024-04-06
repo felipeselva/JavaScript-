8bc35271cc987f22ad6b12e70d4ae2a45994ec25
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-04-06 20:12:28 -0300

    Getters e Setters

---
+++ Objetos e prototypes/aula03.js
@@ -0,0 +1,31 @@
+            // GETTERS E SETTERS 
+
+function Produto(nome,preco,estoque) {
+    this.nome = nome;
+    this.preco=preco;
+    this.estoque =estoque;
+    
+    let estoqueprivado = estoque;
+    Object.defineProperty(this, 'estoque', {
+        enumerable:true, // --> MOSTRA A CHAVE
+        configurable : true,// --> TRUE = CONFIGURAR VARIAVEL  
+        get: () => { return estoqueprivado;},
+
+        set: function(valor) {
+            
+            if (typeof valor !== 'number') {
+                console.log('Valor Invalido'); 
+                return;
+            }
+        
+            estoqueprivado = valor;
+        }
+        });
+    }
+
+
+    
+    const p1 = new Produto ('Camiseta', 20,3)
+        //console.log(p1)
+        p1.estoque = 500;
+        console.log(p1.estoque);
