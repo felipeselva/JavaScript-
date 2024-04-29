86bfb639c7f1b36ec9a5af5e727b0a5ef9809c89
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-04-28 22:43:11 -0300

    Criando usuários (2)

--- Projeto Agenda (com express+MongoDb/Projeto agenda/src/models/LoginModel.js
+++ Projeto Agenda (com express+MongoDb/Projeto agenda/src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator= require('validator');
+const bcryptjs = require('bcryptjs')
 
 
 const LoginSchema = new mongoose.Schema({
@@ -19,13 +20,23 @@ class Login {
   async register() {
     this.valida();
     if(this.errors.length > 0) return;
+    this.userExists()
+    if(this.errors.length > 0) return;
 
     try {
+      const salt = bcryptjs.genSaltSync()
+      this.body.password = bcryptjs.hashSync(this.body.password, salt)
     this.user = await LoginModel.create(this.body)
   } catch(e) {
       console.log(e)
     }
   }
+
+
+async userExists() {
+  const user = await LoginModel.findOne({email: this.body.email})
+  if (user) this.errors.push('Usuario ja exsite')
+}
 
 
   valida() {
