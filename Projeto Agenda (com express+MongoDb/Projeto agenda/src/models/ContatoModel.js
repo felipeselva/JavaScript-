964e5f7b3f0649fcd5e3d9e35f834c1479830073
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-04-30 10:45:17 -0300

    Editando Contatos (Update)

--- Projeto Agenda (com express+MongoDb/Projeto agenda/src/models/ContatoModel.js
+++ Projeto Agenda (com express+MongoDb/Projeto agenda/src/models/ContatoModel.js
@@ -54,7 +54,12 @@ Contato.prototype.cleanUp = function() {
 }
 
 
-
+Contato.prototype.edit = async function(id) {
+  if(typeof id !== 'string') return;
+  this.valida();
+  if(this.erros.length > 0) return;
+  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true});
+}
 
    
 
