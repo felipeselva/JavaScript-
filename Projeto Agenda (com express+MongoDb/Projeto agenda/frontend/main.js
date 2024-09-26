5f4c40e644d26036e7a010e1cdd87756fd5f1ab0
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-09-26 15:20:57 -0300

    .

--- Projeto Agenda (com express+MongoDb/Projeto agenda/frontend/main.js
+++ Projeto Agenda (com express+MongoDb/Projeto agenda/frontend/main.js
@@ -1,1 +1,13 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+//import './assets/style.css';
+
+import Login from './modules/Login';
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+
+login.init();
+cadastro.init();
+
+
 
